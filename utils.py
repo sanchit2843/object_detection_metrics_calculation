cbcf91f38fd30353294ecc60d9b14378aff58168
sanchit2843 <sanchittanwar75@gmail.com>
2021-05-19 16:10:42 +0530

    output path added in write_text

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import os
 import cv2
 
 
-def write_txt(gt, pred, decoder, image_name):
+def write_txt(gt, pred, decoder, image_name,output_path):
     """[This code will can be used to write detected and ground truth metrics in the format used in object detection metrics calculation pipeline]
 
     Text file format is:
@@ -14,7 +14,9 @@ def write_txt(gt, pred, decoder, image_name):
         pred ([tuple]): (detected boxes, predicted classes, predicted scores)
         decoder ([dict]): a dictionary mapping class id with class name.
         image_name ([str]): name of image
+        output_path ([str]): path to output folder, two sub folders groundtruths and detections will be created inside this folder
     """
+
     gt_boxes, gt_classes = gt
     # gt boxes format: [[x1,y1,x2,y2]] a list of bounding box coordinates in format <left> <top> <right> <bottom>
     # gt classes format: A list of predicted classes(integer) for each box. [1,3,0,2]
@@ -24,8 +26,12 @@ def write_txt(gt, pred, decoder, image_name):
     # pred classes format: A list of predicted classes(integer) for each box. [1,3,0,2]
     # pred scores format: A list of predicted confidence scores(float) for each box. [0.8,0.6,0.9]
 
-    f_gt = open("./result_evaluation/groundtruths/{}.txt".format(image_name), "w+")
-    f_pred = open("./result_evaluation/detections/{}.txt".format(image_name), "w+")
+    f_gt = open(
+        os.path.join(output_path, "groundtruths/{}.txt".format(image_name)), "w+"
+    )
+    f_pred = open(
+        os.path.join(output_path, "detections/{}.txt".format(image_name)), "w+"
+    )
 
     for b, c in zip(gt_boxes, gt_classes):
 
